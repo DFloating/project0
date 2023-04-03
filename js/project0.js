191073056b52d5456de61bdf4d2d6d904b8fb690
Dineth Rajapakse <dineth.r1@outlook.com>
2023-04-03 14:54:11 +1000

    Second Commit

---
+++ js/project0.js
@@ -0,0 +1,8 @@
+//  create board in html/css
+// 9 cells
+// X or O to appear once clicked in cells and print/stay
+// alternate between X and O
+// once all cells are filled, message to pop up '' win or Draw 
+// restart button  
+// ^ to clear all X,O's from the board 
+// ^ clear the message pop up for win or draw 
