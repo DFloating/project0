a73b13fb9f340c61e37faa91b45dbd1d6babf0f5
Dineth Rajapakse <dineth.r1@outlook.com>
2023-04-03 16:55:50 +1000

    Third Commit

--- js/project0.js
+++ js/project0.js
@@ -2,7 +2,17 @@
 // 9 cells
 // X or O to appear once clicked in cells and print/stay
 // alternate between X and O
+//winning combinations 
 // once all cells are filled, message to pop up '' win or Draw 
 // restart button  
 // ^ to clear all X,O's from the board 
 // ^ clear the message pop up for win or draw 
+
+const playerX = 'x'
+const playerO = 'circle'
+
+//winning combinations cells on table
+
+// [0, 1, 2] , [3, 4, 5], [ 6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]
+
+
