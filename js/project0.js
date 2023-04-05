c6b9a71f870430961c5c019e64d705aeb0922726
Dineth Rajapakse <dineth.r1@outlook.com>
2023-04-05 20:35:33 +1000

    Fourth Commit

--- js/project0.js
+++ js/project0.js
@@ -7,12 +7,37 @@
 // restart button  
 // ^ to clear all X,O's from the board 
 // ^ clear the message pop up for win or draw 
+document.querySelectorAll(".box");
+document.getElementById("board");
 
-const playerX = 'x'
-const playerO = 'circle'
+// global variables to control game
+let currentPlayer = 'X';
+let gameActive = true;
+let board = [" ", " ", " ", " ", " ", " ", " ", " ",  " "];
 
-//winning combinations cells on table
 
-// [0, 1, 2] , [3, 4, 5], [ 6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]
+
+
+//  need event listener for each cell for when the user clicks on the cell //event paramater
+// for loop?
+
+
+// //winning combinations cells on table
+const winningCombos = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [2, 4, 6], 
+    [0, 4, 8]
+]
+// // [0, 1, 2] , [3, 4, 5], [ 6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]
+ // if winningCombos === false return 'message'
+
+
+
+// if statement to change the HTML to swap the symbol from X to O (if, else)
 
 
