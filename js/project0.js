67e4048e7bf3e4719c21fbbb99bf10b1304cb38d
Dineth Rajapakse <dineth.r1@outlook.com>
2023-04-06 08:14:48 +1000

    Fifth Commit

--- js/project0.js
+++ js/project0.js
@@ -7,21 +7,43 @@
 // restart button  
 // ^ to clear all X,O's from the board 
 // ^ clear the message pop up for win or draw 
-document.querySelectorAll(".box");
-document.getElementById("board");
 
+const statusDisplay = document.querySelector('.statusText');
 // global variables to control game
 let currentPlayer = 'X';
 let gameActive = true;
 let board = [" ", " ", " ", " ", " ", " ", " ", " ",  " "];
 
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.restartButton').addEventListener('click', handleRestartGame);
 
 
 //  need event listener for each cell for when the user clicks on the cell //event paramater
 // for loop?
+function handleCellClick(clickedCellEvent) {   
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(
+      clickedCell.getAttribute('data-cell-index')
+    );
 
+    if (board[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
 
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+   
+};
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    
+    board[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
 // //winning combinations cells on table
 const winningCombos = [
     [0, 1, 2], 
@@ -41,3 +63,11 @@ const winningCombos = [
 // if statement to change the HTML to swap the symbol from X to O (if, else)
 
 
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    // statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell')
+               .forEach(cell => cell.innerHTML = "");
+} 
