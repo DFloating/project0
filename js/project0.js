48c87e4c51c02922c4778b4fc6ac207d62f8beea
Dineth Rajapakse <dineth.r1@outlook.com>
2023-04-10 14:00:09 +1000

    lets go

--- js/project0.js
+++ js/project0.js
@@ -1,118 +1,46 @@
-// //  create board in html/css
-// // 9 cells
-// // X or O to appear once clicked in cells and print/stay
-// // alternate between X and O
-// //winning combinations 
-// // once all cells are filled, message to pop up '' win or Draw 
-// // restart button  
-// // ^ to clear all X,O's from the board 
-// // ^ clear the message pop up for win or draw 
-
-// const statusDisplay = document.querySelector('.statusText');
-// // global variables to control game
-// let currentPlayer = 'X';
-// let gameActive = true;
-// let board = [" ", " ", " ", " ", " ", " ", " ", " ",  " "];
-
 // const winningMessage = () => `Player ${currentPlayer} has won!`;
 // const drawMessage = () => `Game ended in a draw!`;
 // const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
-// // document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
-// // document.querySelector('.restartButton').addEventListener('click', handleRestartGame);
-
-
-// //  need event listener for each cell for when the user clicks on the cell //event paramater
-// // for loop?
-// // function handleCellClick(clickedCellEvent) {   
-// //     const clickedCell = clickedCellEvent.target;
-// //     const clickedCellIndex = parseInt(
-// //       clickedCell.getAttribute('data-cell-index')
-// //     );
-
-// //     if (board[clickedCellIndex] !== "" || !gameActive) {
-// //         return;
-// //     }
-
-// //     handleCellPlayed(clickedCell, clickedCellIndex);
-// //     handleResultValidation();
-   
-// // };
-// function handleCellClick(event) {
-//     const clickedCell = event.target;
-//     const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
-
-    
-//     if (board[clickedCellIndex] !== "" || !gameActive) {
-//         return;
-//     }
-
-    
-//     handleCellPlayed(clickedCell, clickedCellIndex);
-
-    
-//     handleResultValidation();
-
-
-//     currentPlayer = currentPlayer === "X" ? "O" : "X";
-// }
-
-
-
-// // function handleCellPlayed(clickedCell, clickedCellIndex) {
-    
-// //     board[clickedCellIndex] = currentPlayer;
-// //     clickedCell.innerHTML = currentPlayer;
-// // }
-// // //winning combinations cells on table
-// const winningCombos = [
-//     [0, 1, 2], 
-//     [3, 4, 5], 
-//     [6, 7, 8], 
-//     [0, 3, 6], 
-//     [1, 4, 7], 
-//     [2, 5, 8], 
-//     [2, 4, 6], 
-//     [0, 4, 8]
-// ]
-// // // [0, 1, 2] , [3, 4, 5], [ 6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]
-//  // if winningCombos === false return 'message'
-
-
-
-// // if statement to change the HTML to swap the symbol from X to O (if, else)
-
-
 
 const cells = document.querySelectorAll('.cell');
+const statusText = document.querySelector('.statusText')
+const restartButton = document.querySelector('.restartButton');
 
 // Initialize variables
 let currentPlayer = 'X';
 let gameEnded = false;
+// let options = ["", "", "", "", "", "", "", "", ""];
 
 // Add event listener to each cell
-
-for (let i = 0; i < cells.length; i++) { //Run a for Loop for grid. tar
+function startGame() {
+for (let i = 0; i < cells.length; i++) { //Run a for Loop for grid of cells
     cells[i].addEventListener('click', () => { //Adding event listener to each individual cell. 
         if (!gameEnded && cells[i].textContent === '') { // If gameEnded === false, && the indivudal cell that we click text content is not occupied by anything
         cells[i].textContent = currentPlayer; // occupy with currentPlayer
         if (checkForWin()) { //calling below function, checking for winner
-            alert(`${currentPlayer} won!`); // alerting that player has won
-            gameEnded = true; // we are setting the gameEnded to true which doesn't allow any further boxes to be click 
-        } else if (checkForDraw()) { //calling below function to check for draw
-            alert('Draw!');
+            console.log(`${currentPlayer} won!`); // alerting that player has won
             gameEnded = true;
+            statusText.textContent = `${currentPlayer} won!`
+             // we are setting the gameEnded to true which doesn't allow any further boxes to be click 
+        } else if (checkForDraw()) { //calling below function to check for draw
+            console.log('Draw!');
+            gameEnded = true;
+            statusText.textContent = `Draw!`
         } else {
             if (currentPlayer === "X") { //After click, checking to see if player1 is current player,
                 currentPlayer = "O"; //if so, switch to player 2
               } else {
                 currentPlayer = "X"; //opposite of above 
               }
-        }
-        }
+              statusText.textContent = `${currentPlayer}'s turn!`
+        };
+        };
+        restartButton.addEventListener("click", restartGame);
     });
+    
+};
 }
-
 
 // Check if there is a win
 function checkForWin() { 
@@ -132,6 +60,7 @@ function checkForWin() {
     const [a, b, c] = combo; // Array destructuring, assigning values to a, b and c in relevance to the arrays nested in the WinningCombo Array
     return cells[a].textContent !== '' && cells[a].textContent === cells[b].textContent && cells[b].textContent === cells[c].textContent; //checking to see if the first index position is not empty, and the other index positions are equal to the first in terms of text content.
   });
+//    statusText.textContent = `${currentPlayer} won!`;
 }
 
 // Check if there is a draw
@@ -140,24 +69,22 @@ function checkForDraw() {
 }
 //creating a new array, and with the every, we're checking every index position with a loop, and then we're checking to see if the textContent is not empty for all of them, and a winning condition hasn't been met. 
 
+// document.querySelector('.restartButton').addEventListener('click', handleRestartGame);
 
 
 
 
+function restartGame() {
+   
+    
+    cells.forEach(cell => cell.textContent = '');
+
+               
+} ;
 
 
-
-
-
-
-// function handleRestartGame() {
-//     gameActive = true;
-//     currentPlayer = "X";
-//     gameState = ["", "", "", "", "", "", "", "", ""];
-//     // statusDisplay.innerHTML = currentPlayerTurn();
-//     document.querySelectorAll('.cell')
-//                .forEach(cell => cell.innerHTML = "");
-// } 
 
 // document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
 // document.querySelector('.restartButton').addEventListener('click', handleRestartGame);
+
+startGame();
